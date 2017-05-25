bfee6058da2490c94bde77e8864e3be08259d8fa
Christian650 <cjimenez.sp@mysummitps.org>
2017-05-25 19:05:59 +0000

    upadate

---
+++ changer.js
@@ -0,0 +1,10 @@
+//Creates Variables and Saves DOM Elements to each
+var newBGColor = document.getElementById("newBGColor");
+var colorChangeButton = document.getElementById("colorChangeButton");
+
+
+colorChangeButton.addEventListener("click",function(){
+ document.body.style.backgroundColor=newBGColor.value;   
+})
+
+
